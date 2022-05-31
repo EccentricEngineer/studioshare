eaa3a773c4513a01b969bfeb4c3934f72b67278a
Muhammad Ali <ali648@live.com>
2022-05-31 16:58:33 +0500

    validations & associations done

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :reservation
   belongs_to :user
   belongs_to :studio
+
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reviews
+  has_many :reservations
+
+  validates_length_of :password, in: 6..20, on: :create
+  validates :password, presence: true
 end

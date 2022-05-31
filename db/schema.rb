fea804c4b3b4f27bb24cb9b6acfa2789de935769
Muhammad Ali <ali648@live.com>
2022-05-31 20:36:07 +0500

    studio and user model altered

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_095118) do
+ActiveRecord::Schema.define(version: 2022_05_31_153051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,25 +43,13 @@ ActiveRecord::Schema.define(version: 2022_05_31_095118) do
   create_table "studios", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.text "name"
-    t.text "country"
-    t.text "city"
     t.text "address"
-    t.integer "longitude"
-    t.integer "latitude"
-    t.integer "capacity"
-    t.boolean "mixing_console"
-    t.boolean "multitrack_recorder"
-    t.boolean "microphones"
-    t.boolean "reference_monitor"
-    t.boolean "digital_workstation"
-    t.boolean "music_workstation"
-    t.boolean "outboard_effect"
-    t.boolean "isolation_booth"
     t.integer "photo_url"
     t.datetime "opening_hour"
     t.datetime "closing_hour"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_studios_on_user_id"
   end
 
@@ -71,7 +59,6 @@ ActiveRecord::Schema.define(version: 2022_05_31_095118) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "vendor"
     t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Seed: Deleting existing records...'
+
+Review.delete_all
+Reservation.delete_all
+Studio.delete_all
+User.delete_all
+
+puts 'Seed: Seeding...'
+
+user1 = User.create!(
+  name: "Malcolm",
+  vendor: true
+)
+
+user2 = User.create!(
+  name: " Nina",
+  vendor: false
+)
+
+user3 = User.create!(
+  name: "Michael",
+  vendor: true
+)
+
+
+studio1 = Studio.create!(
+  name: "Alexie Studio",
+  user: user1,
+  address: "lane 2, Poland",
+  photo_url: "https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  opening_hour: "2022-05-31 14:22:53.657840837 +0200",
+  closing_hour: "2022-05-31 15:22:53.657840937 +0200"
+)
+
+studio2 = Studio.create!(
+  name: "Ali Studio",
+  user: user2,
+  address: "lane 2, UK",
+  photo_url: "https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  opening_hour: "2022-05-31 14:22:53.657840837 +0200",
+  closing_hour: "2022-05-31 15:22:53.657840937 +0200"
+)
+
+studio3 = Studio.create!(
+  name: "Claudio Studio",
+  user: user3,
+  address: "lane 10, France",
+  photo_url: "https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  opening_hour: "2022-05-31 14:22:53.657850837 +0200",
+  closing_hour: "2022-05-31 15:22:53.657840937 +0200"
+)

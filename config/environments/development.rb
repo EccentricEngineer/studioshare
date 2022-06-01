4819d2c586ceb9b3055da05151137c8657637daa
Claudio Clemente <claudioclemente@hotmail.it>
2022-06-01 20:29:57 +0200

    front-end updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Setting up the devise (from the slides)
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
